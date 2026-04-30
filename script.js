54c40be809a428b77b0aec6ea520b81013974e17
Sanket <sanket@email.com>
2026-04-30 20:58:57 +0530

    Updated script.js with fetch API changes

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const cartButtons = document.querySelectorAll(".addToCart");
 cartButtons.forEach((btn, index) => {
     btn.addEventListener("click", () => {
  fetch("https://fakestoreapi.com/carts", {
+      method: "POST",
+            headers: {
