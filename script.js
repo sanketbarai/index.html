ca473626776cb8ca17b8f784be8ad084241e50cc
Sanket <sanket@email.com>
2026-04-30 21:08:46 +0530

    Completed fetch POST request with JSON headers and review data body submission

--- script.js
+++ script.js
@@ -34,3 +34,10 @@ form.addEventListener("submit", function(e) {
     fetch("https://jsonplaceholder.typicode.com/posts", {
         method: "POST",
         headers: {
+             "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: name,
+            review: review
+        })
+    })
