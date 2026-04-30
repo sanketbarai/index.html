f716199ee2e37ae3d67617a28dcdb16587c1decc
Sanket <sanket@email.com>
2026-04-30 20:58:16 +0530

    Added fetch API for cart functionality

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const cartButtons = document.querySelectorAll(".addToCart");
 
 cartButtons.forEach((btn, index) => {
     btn.addEventListener("click", () => {
+ fetch("https://fakestoreapi.com/carts", {
